b870e819c2b7e905ebb2ebaa7f6be0e4649d6424
rctinstructor <76188681+rctinstructor@users.noreply.github.com>
2021-01-09 12:34:31 -0500

    Add project files.

---
+++ EmployeeManagement/Migrations/20190430055132_AlterEmployeesSeedData.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeManagement.Migrations
+{
+    public partial class AlterEmployeesSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Email", "Name" },
+                values: new object[] { "joana@gmail.com", "joana" });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "Department", "Email", "Name" },
+                values: new object[] { 2, 1, "carlos@gmail.com", "carlos" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Email", "Name" },
+                values: new object[] { "mark@gmail.com", "Mark" });
+        }
+    }
+}
---
+++ EmployeeManagement/Models/ModelBuilderExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().HasData(
+                    new Employee
+                    {
+                        Id = 1,
+                        Name = "joana",
+                        Department = Dept.IT,
+                        Email = "joana@gmail.com"
+                    },
+                    new Employee
+                    {
+                        Id = 2,
+                        Name = "carlos",
+                        Department = Dept.HR,
+                        Email = "carlos@gmail.com"
+                    }
+                );
+        }
+    }
+}
